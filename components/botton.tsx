6fff14427b217ca360e3978b4cfa6e4a90a5f38d
Alan Agila <acr.aila@yavirac.edu.ec>
2025-07-16 16:50:33 -0500

    feat: crear boton reutilizable

---
+++ components/botton.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+type NavigationButtonProps = {
+  to: string;
+  label: string;
+};
+
+export default function NavigationButton({ to, label }: NavigationButtonProps) {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push(to);
+  }
+
+  return (
+    <button
+      onClick={handleClick}
+      className="px-6 py-3 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+    >
+      {label}
+    </button>
+  );
+}
